9022966242ee5a4b38d5ba94a6fe2042a50e2ee0
djazayeri <jazayeri@alum.mit.edu>
2013-06-04 22:31:28 -0700

    App descriptor and test case for a "find patient" app, for App Framework tutorial

---
+++ omod/src/test/java/org/openmrs/module/coreapps/fragment/controller/visit/AppTest.java
@@ -0,0 +1,21 @@
+package org.openmrs.module.coreapps.fragment.controller.visit;
+
+import org.junit.Test;
+import org.openmrs.module.appframework.AppTestUtil;
+import org.openmrs.module.appframework.domain.AppDescriptor;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ */
+public class AppTest {
+
+    @Test
+    public void testFindPatientAppIsLoaded() throws Exception {
+        AppDescriptor app = AppTestUtil.getAppDescriptor("coreapps.findPatient");
+        assertThat(app.getOrder(), is(2));
+    }
+
+}
