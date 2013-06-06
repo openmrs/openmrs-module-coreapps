14fcd1fe03311f51b67ba0e9481e16381c09ab45
djazayeri <jazayeri@alum.mit.edu>
2013-06-05 18:13:16 -0700

    Missed some changes to encounterTemplates.js file when extracting the patient dashboard

--- omod/src/main/webapp/resources/scripts/fragments/encounterTemplates.js
+++ omod/src/main/webapp/resources/scripts/fragments/encounterTemplates.js
@@ -1,42 +1,42 @@
 //Uses the namespace pattern from http://stackoverflow.com/a/5947280
 (function( encounterTemplates, $, undefined) {
-		
-	var templates = {};
-	var defaultTemplate;
-	var parameters = {};
-	
-	encounterTemplates.setTemplate = function(encounterTypeUuid, templateId) {
-		templates[encounterTypeUuid] = _.template(jq('#' + templateId).html());
-	};
-	
-	encounterTemplates.setDefaultTemplate = function(templateId) {
-		defaultTemplate = _.template(jq('#' + templateId).html());
-	};
-	
-	encounterTemplates.setParameter = function(encounterTypeUuid, name, value) {
-		if (!(encounterTypeUuid in parameters)) {
-			parameters[encounterTypeUuid] = {};
-		}
-		parameters[encounterTypeUuid][name] = value;
-	};
 
-	encounterTemplates.displayEncounter = function(encounter) {
-		var template;
-		if (templates[encounter.encounterType.uuid]) {
-			template = templates[encounter.encounterType.uuid];
-		} else {
-			template = defaultTemplate;
-		}
-		
-		var data = {};
-		if (parameters[encounter.encounterType.uuid]) {
-			data = parameters[encounter.encounterType.uuid];
-		} else {
-			data.icon = "icon-time";
-		}
-		data.encounter = encounter;
-		
-		return template(data);
-	};
-	
+    var templates = {};
+    var defaultTemplate;
+    var parameters = {};
+
+    encounterTemplates.setTemplate = function(encounterTypeUuid, templateId) {
+        templates[encounterTypeUuid] = _.template(jq('#' + templateId).html());
+    };
+
+    encounterTemplates.setDefaultTemplate = function(templateId) {
+        defaultTemplate = _.template(jq('#' + templateId).html());
+    };
+
+    encounterTemplates.setParameter = function(encounterTypeUuid, name, value) {
+        if (!(encounterTypeUuid in parameters)) {
+            parameters[encounterTypeUuid] = {};
+        }
+        parameters[encounterTypeUuid][name] = value;
+    };
+
+    encounterTemplates.displayEncounter = function(encounter, patient) {
+        var template;
+        if (templates[encounter.encounterType.uuid]) {
+            template = templates[encounter.encounterType.uuid];
+        } else {
+            template = defaultTemplate;
+        }
+
+        var data = {
+            encounter: encounter,
+            patient: patient
+        };
+        data.config = _.extend({
+            icon: "icon-time"
+        }, parameters[encounter.encounterType.uuid]);
+
+        return template(data);
+    };
+
 }( window.encounterTemplates = window.encounterTemplates || {}, jQuery ));
