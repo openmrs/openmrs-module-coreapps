988a1cae70a1da9e7e23bc491648c3845129d689
djazayeri <jazayeri@alum.mit.edu>
2013-06-07 15:30:29 -0700

    Added homepage link (and test case) to "find patient" app, for App Framework tutorial

--- omod/src/test/java/org/openmrs/module/coreapps/AppTest.java
+++ omod/src/test/java/org/openmrs/module/coreapps/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openmrs.module.appframework.AppTestUtil;
 import org.openmrs.module.appframework.domain.AppDescriptor;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -16,6 +17,13 @@ public class AppTest {
     public void testFindPatientAppIsLoaded() throws Exception {
         AppDescriptor app = AppTestUtil.getAppDescriptor("coreapps.findPatient");
         assertThat(app.getOrder(), is(2));
+    }
+
+    @Test
+    public void testFindPatientAppHasHomepageExtension() throws Exception {
+        AppDescriptor app = AppTestUtil.getAppDescriptor("coreapps.findPatient");
+        assertThat(app.getExtensions(), hasSize(1));
+        assertThat(app.getExtensions().get(0).getExtensionPointId(), is("org.openmrs.referenceapplication.homepageLink"));
     }
 
 }
