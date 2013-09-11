8f4dbb4e48eb99959e4e9ab5993655d8c57c2d87
wluyima <wyclif@openmrs.org>
2013-09-10 23:27:56 -0400

    Improve UI/UX of Patient Lookup - RA-199
    Removed some log messages and improved the behavior of the keyboard row highlight - RA-199
    
    Converted all widget members to be private - RA-199
    
    Made more refactorings in the code - RA-199
    
    Added support for selecting rows by pressing enter, also added moment.js for formatting dates - RA-199
    
    Added support for performing searches as the user types - RA-199
    
    Add message translations for the widget and datatable labels - RA-199
    
    Applied some ui improvements - RA-199
    
    Hide the search results div on page load - RA-199
    
    Removed log messages - RA-199
    
    Added moment.js file - RA-199
    
    Removed unused script include - RA-199

--- omod/src/main/webapp/pages/findpatient/findPatient.gsp
+++ omod/src/main/webapp/pages/findpatient/findPatient.gsp
@@ -1,6 +1,10 @@
 <%
     ui.decorateWith("appui", "standardEmrPage")
     ui.includeCss("coreapps", "findpatient/findPatient.css")
+    ui.includeCss("uicommons", "datatables/dataTables_jui.css")
+    ui.includeJavascript("uicommons", "datatables/jquery.dataTables.min.js")
+    ui.includeJavascript("coreapps", "findpatient/findPatient.js")
+    ui.includeJavascript("coreapps", "moment.min.js")
 %>
 <script type="text/javascript">
     var breadcrumbs = [
@@ -9,56 +13,51 @@
     ];
 
     jq(function() {
-        var resultTemplate = _.template(jq('#result-template').html());
-
-        // this is a quick hack -- we really want to autosearch after they type a few keys
-        jq('#patient-search-form').submit(function() {
-            var query = jq('#patient-search').val();
-            var customRep = 'custom:(uuid,identifiers:(identifierType:(name),identifier),person)';
-            jq.getJSON('/' + OPENMRS_CONTEXT_PATH + '/ws/rest/v1/patient', {v: customRep, q: query }, function(data) {
-                var resultTarget = jq('#patient-search-results');
-                resultTarget.html('');
-                _.each(data.results, function(patient) {
-                    var url = '/' + OPENMRS_CONTEXT_PATH + emr.applyContextModel('${ ui.escapeJs(afterSelectedUrl) }', { patientId: patient.uuid });
-                    resultTarget.append(resultTemplate({ patient: patient, url: url }));
-                });
-            });
-            return false;
-        });
-
+        new PatientSearchWidget(getConfig());
         jq('#patient-search').focus();
-
-        jq('#patient-search-results').on('click', '.patient-search-result', function(evt) {
-            location.href = jq(this).find('a.button').attr('href');
-        });
     });
+
+    function getConfig(){
+        var config = {
+            searchInputId: 'patient-search',
+            searchButtonId: 'patient-search-button',
+            searchResultsTableId: 'patient-search-results-table',
+            searchResultsDivId: 'patient-search-results',
+            afterSelectedUrl: '${ ui.escapeJs(afterSelectedUrl) }',
+            messages: {
+                info: '${ ui.message("coreapps.search.info") }',
+                first: '${ ui.message("coreapps.search.first") }',
+                previous: '${ ui.message("coreapps.search.previous") }',
+                next: '${ ui.message("coreapps.search.next") }',
+                last: '${ ui.message("coreapps.search.last") }',
+                noMatchesFound: '${ ui.message("coreapps.search.noMatchesFound") }'
+            }
+        };
+
+        return config;
+    }
 </script>
 
 <h2>
-	${ ui.message(heading) }
+	${ ui.message('coreapps.findPatient.app.label') }
 </h2>
-    
-<form method="get" id="patient-search-form">
+
+<form method="get" id="patient-search-form" onsubmit="return false">
     <input type="text" id="patient-search" placeholder="${ ui.message("coreapps.findPatient.search.placeholder") }" autocomplete="off"/>
-    <input type="submit" value="${ ui.message("coreapps.findPatient.search.button") }"/>
+    <input type="button" id="patient-search-button" value="${ ui.message("coreapps.findPatient.search.button") }" />
 </form>
 
-<ul id="patient-search-results">
-</ul>
-
-<script type="text/template" id="result-template">
-    <li class="patient-search-result" data-patient-uuid="{{- patient.uuid }}">
-        {{ _.each(patient.identifiers, function(id) { }}
-            <span class="patient-identifier">
-                <span class="identifier-type">{{- id.identifierType.name }}</span>
-                <span class="identifier">{{- id.identifier }}</span>
-            </span>
-        {{ }) }}
-        <span class="preferred-name">
-            {{- patient.person.preferredName.display }}
-        </span>
-        <span class="age">{{- patient.person.age }}</span>
-        <span class="gender">{{- patient.person.gender }}</span>
-        <a class="button" href="{{= url }}">${ ui.message("coreapps.findPatient.result.view") }</a>
-    </li>
-</script>
+<div id="patient-search-results">
+    <table id="patient-search-results-table">
+        <thead>
+            <tr>
+                <th>${ ui.message("coreapps.search.identifier") }</th>
+                <th>${ ui.message("coreapps.search.name") }</th>
+                <th>${ ui.message("coreapps.age") }</th>
+                <th>${ ui.message("coreapps.gender") }</th>
+                <th>${ ui.message("coreapps.birthdate") }</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</div>
---
+++ omod/src/main/webapp/resources/scripts/findpatient/findPatient.js
@@ -0,0 +1,344 @@
+function PatientSearchWidget(configuration){
+    var config = configuration;
+    var input = jq('#'+config.searchInputId);
+    var pageCount = 0;
+    var searchResultsData = [];
+    var highlightedKeyboardRowIndex;
+    var highlightedMouseRowIndex;
+    var searchDelayShort = 300;
+    var searchDelayLong = 1000;
+    var searchDelayTimer;
+    var url = '/' + OPENMRS_CONTEXT_PATH + '/ws/rest/v1/patient';
+    //UP(38), DOWN(40), PAGE UP(33), PAGE DOWN(34)
+    var keyboardNavigationKeys = [33,34,38,40];
+    /*
+     *SHIFT(16), CTRL(17), ALT(18), CAPS LOCK(20), ESC(27), END(35), HOME(36), LEFT(37), RIGHT(39)
+     *MAC COMMAND KEYs differ by browser
+     *  Firefox: 224
+     *  Opera: 17(CTRL equivalent)
+     *  WebKit (Safari/Chrome): 91 (Left command key) or 93 (Right command key)
+     */
+    var keyboardControlKeys = [16,17,18,20,27,35,36,37,39,91,93,224];
+    var customRep = 'custom:(uuid,' +
+                    'patientIdentifier:(uuid,identifier),' +
+                    'person:(gender,age,birthdate,personName:(fullName)))';
+
+    var doSearch = function(query){
+        reset();
+        if(!jq('#'+config.searchResultsDivId).is(':visible')){
+            jq('#'+config.searchResultsDivId).show();
+        }
+        query = jq.trim(query);
+        jq.getJSON(url, {v: customRep, q: query }, function(data) {
+            if(data){
+                updateSearchResults(data.results);
+            }
+        });
+    }
+
+    var reset = function(){
+        dTable.fnClearTable();
+        searchResultsData = [];
+        pageCount = 0;
+        highlightedKeyboardRowIndex = undefined;
+        highlightedMouseRowIndex = undefined;
+    }
+
+    var updateSearchResults = function(results){
+        searchResultsData = results;
+        var dataRows = [];
+        _.each(searchResultsData, function(patient, index) {
+            var birthdate = '';
+            if(patient.person.birthdate){
+                birthdate = moment(patient.person.birthdate).format('DD-MMM-YYYY');
+            }
+            dataRows.push([patient.patientIdentifier.identifier, patient.person.personName.fullName,
+                patient.person.age, patient.person.gender, birthdate]);
+        });
+
+        dTable.fnAddData(dataRows);
+
+        refreshTable();
+    }
+
+    var refreshTable = function(){
+        var rowCount = searchResultsData.length;
+        if(rowCount % dTable.fnSettings()._iDisplayLength == 0){
+            pageCount = rowCount/dTable.fnSettings()._iDisplayLength;
+        }else{
+            pageCount = Math.floor(rowCount/dTable.fnSettings()._iDisplayLength)+1;
+        }
+    }
+
+    var isTableEmpty = function(){
+        return !dTable || dTable.fnGetNodes().length == 0;
+    }
+
+    var selectRow = function(selectedRowIndex){
+        var uuid = searchResultsData[selectedRowIndex].uuid;
+        location.href = '/' + OPENMRS_CONTEXT_PATH + emr.applyContextModel(config.afterSelectedUrl, { patientId: uuid});
+    }
+
+    var doKeyEnter = function() {
+        if (highlightedKeyboardRowIndex == undefined){
+            clearTimeoutIfNecessary();
+            doSearch(input.val());
+            return;
+        }
+        selectRow(highlightedKeyboardRowIndex);
+    }
+
+    var clearTimeoutIfNecessary = function(){
+        //if there is any search delay in progress, cancel it
+        if(searchDelayTimer != undefined){
+            window.clearTimeout(searchDelayTimer);
+        }
+    }
+
+    var doKeyDown = function() {
+        //the user is using the mouse and they also want to use up/down keys?, dont support this
+        if(highlightedMouseRowIndex)
+            return;
+
+        var prevRow = highlightedKeyboardRowIndex;
+        //if we are on the last page, and the last row is highlighted, do nothing
+        if(getCurrVisiblePage() == pageCount && prevRow >= (searchResultsData.length-1))
+            return;
+
+        //only move the highlight to next row if it is currently on the visible page otherwise should be on first row
+        if(isHighlightedRowOnVisiblePage()){
+            highlightedKeyboardRowIndex++;
+
+            //If the selected row is the first one on the next page, flip over to its page
+            if(highlightedKeyboardRowIndex != 0 && (highlightedKeyboardRowIndex % dTable.fnSettings()._iDisplayLength) == 0) {
+                dTable.fnPageChange('next');
+            }
+        }
+
+        if(prevRow != undefined) {
+            unHighlightRow(dTable.fnGetNodes()[prevRow]);
+        }
+
+        highlightRow();
+    }
+
+    var doKeyUp = function() {
+        if(highlightedMouseRowIndex)
+            return;
+
+        //we are on the first page and the first row is already highlighted, do nothing
+        if(dTable.fnSettings()._iDisplayStart == 0 && highlightedKeyboardRowIndex == 0)
+            return;
+
+        var prevRow = highlightedKeyboardRowIndex;
+        //only move the highlight to prev row if it is currently on the visible page otherwise shoule be last row
+        if(isHighlightedRowOnVisiblePage()){
+            highlightedKeyboardRowIndex--;
+            if(prevRow != undefined) {
+                if(prevRow % dTable.fnSettings()._iDisplayLength == 0)
+                    dTable.fnPageChange('previous');
+
+                unHighlightRow(dTable.fnGetNodes()[prevRow]);
+            }
+        }else{
+            //user just flipped pages, highlight the last row on the currently visible page
+            if(getCurrVisiblePage() < pageCount){
+                highlightedKeyboardRowIndex = dTable.fnSettings()._iDisplayStart + dTable.fnSettings()._iDisplayLength - 1;
+            }else{
+                //this is the last page, highlight the last item in the table
+                highlightedKeyboardRowIndex = searchResultsData.length-1;
+            }
+        }
+
+        highlightRow();
+    }
+
+    var doPageDown = function() {
+        dTable.fnPageChange('next');
+        //if this is the last page and we already have a selected row, do nothing
+        if( (getCurrVisiblePage() == pageCount) && highlightedKeyboardRowIndex > dTable.fnSettings()._iDisplayStart)
+            return;
+
+        //update the highlightest row to the first on the new page
+        //highlightRow();
+        highlightedKeyboardRowIndex = undefined;
+    }
+
+    var doPageUp = function() {
+        dTable.fnPageChange('previous');
+        if(isHighlightedRowOnVisiblePage())
+            return;
+
+        //update the highlight to go to last row on previous page
+        //highlightRow();
+        highlightedKeyboardRowIndex = undefined;
+    }
+
+    /**
+     * Highlights the row at the index that matches the value of 'highlightedKeyboardRowIndex' otherwise the
+     * first on the current visible page
+     */
+    var highlightRow = function(){
+        //the row to highlight has to be on the visible page, this helps not to lose the highlight
+        //when the user uses the pagination buttons(datatables provides no callback function)
+        if(!isHighlightedRowOnVisiblePage()){
+            //highlight the first row on the currently visible page
+            highlightedKeyboardRowIndex = dTable.fnSettings()._iDisplayStart;
+        }
+        var highlightedRowNode = dTable.fnGetNodes()[highlightedKeyboardRowIndex];
+        jq(highlightedRowNode).addClass('search_table_row_highlight');
+    }
+
+    /**
+     * Removes the highlight from the specified row
+     */
+    var unHighlightRow = function(row){
+        jq(row).removeClass("search_table_row_highlight");
+    }
+
+    /**
+     * Returns true if the row highlight is on the current visible page
+     */
+    var isHighlightedRowOnVisiblePage = function(){
+        return highlightedKeyboardRowIndex != undefined && (highlightedKeyboardRowIndex >= dTable.fnSettings()._iDisplayStart)
+            && (highlightedKeyboardRowIndex < (dTable.fnSettings()._iDisplayStart + dTable.fnSettings()._iDisplayLength));
+    }
+
+    /**
+     * Gets the current page the user is viewing in the datatable
+     */
+    var getCurrVisiblePage = function(){
+        return Math.ceil(dTable.fnSettings()._iDisplayStart / dTable.fnSettings()._iDisplayLength) + 1;
+    }
+
+    /********************* INITIALIZATION DATATABLE *********************/
+
+    var dTable = jq('#'+config.searchResultsTableId).dataTable({
+        bFilter: false,
+        bJQueryUI: true,
+        bLengthChange: false,
+        iDisplayLength: 15,
+        sPaginationType: "full_numbers",
+        bSort: false,
+        sDom: 't<"fg-toolbar ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix"ip>',
+        oLanguage: {
+            "sInfo": config.messages.info,
+            "sZeroRecords": config.messages.noMatchesFound,
+            "oPaginate": {
+                "sFirst": config.messages.first,
+                "sPrevious": config.messages.previous,
+                "sNext": config.messages.next,
+                "sLast": config.messages.last
+            }
+        },
+
+        fnDrawCallback : function(oSettings){
+            if(isTableEmpty()){
+                //this should ensure that nothing happens when the use clicks the
+                //row that contain the text that says 'No data available in table'
+                return;
+            }
+
+            if(highlightedKeyboardRowIndex != undefined && !isHighlightedRowOnVisiblePage()){
+                unHighlightRow(dTable.fnGetNodes(highlightedKeyboardRowIndex));
+            }
+
+            //fnDrawCallback is called on each page redraw so we need to remove any previous handlers
+            //otherwise there will multiple hence the logic getting executed multiples times as the
+            //user the goes back and forth between pages
+            dTable.$('tr').unbind('click');
+            dTable.$('tr').unbind('hover');
+
+            dTable.$('tr').click(
+                function(){
+                    highlightedMouseRowIndex = dTable.fnGetPosition(this);
+                    selectRow(highlightedMouseRowIndex);
+                }
+            );
+
+            dTable.$('tr').hover(
+                function(){
+                    if(highlightedKeyboardRowIndex){
+                        var keyboardHighlightedNode = dTable.fnGetNodes()[highlightedKeyboardRowIndex];
+                        unHighlightRow(keyboardHighlightedNode);
+                        highlightedKeyboardRowIndex = undefined;
+                    }
+                    highlightedMouseRowIndex = dTable.fnGetPosition(this);
+                }, function(){
+                    highlightedMouseRowIndex = undefined;
+                }
+            );
+
+            //ensure that the input never loses field whenever a user
+            //e.g when user clicks the paging buttons
+            input.focus();
+        }
+    });
+
+    /***************** SETUP KEYBOARD AND MOUSE EVENT HANDLERS **************/
+
+    jq('#'+config.searchButtonId).click(
+        function(){
+            var enterEvent = jQuery.Event("keydown", { keyCode: 13 });
+            input.trigger( enterEvent);
+            input.focus();//ensures key board events handlers are not broken
+        }
+    );
+
+    input.keyup(function(event) {
+        var kc = event.keyCode;
+        //ignore enter(because it was handled already onkeydown), keyboard navigation and control keys
+        if(kc == 13 || _.contains(keyboardNavigationKeys, kc) || _.contains(keyboardControlKeys, kc)) {
+            return false;
+        }
+
+        //if there is any search delay set, cancel it
+        clearTimeoutIfNecessary();
+
+        var text = jq.trim(input.val());
+        if(text.length > 0){
+            // force a longer delay since we are going to search on a shorter string
+            //or may be the user is still typing more characters
+            var effectiveSearchDelay = (text.length > 2) ? searchDelayShort : searchDelayLong;
+
+            //wait for a couple of milliseconds, if the user isn't typing anymore chars before triggering search
+            //this minimizes the number of un-necessary calls made to the server for first typists
+            searchDelayTimer = window.setTimeout(function(){
+                doSearch(text);
+            }, effectiveSearchDelay);
+        }else{
+            reset();
+        }
+
+        return false;
+    });
+
+    //catch control keys to stop the cursor in the input box from moving.
+    input.keydown(function(event) {
+        var kc = event.keyCode;
+        if((kc == 13 || _.contains(keyboardNavigationKeys, kc)) && !isTableEmpty()) {
+
+            switch(kc) {
+                case 13:
+                    doKeyEnter();
+                    break;
+                case 33:
+                    doPageUp();
+                    break;
+                case 34:
+                    doPageDown();
+                    break;
+                case 38:
+                    doKeyUp();
+                    break;
+                case 40:
+                    doKeyDown();
+                    break;
+            }
+
+            return false;
+        }
+
+        return true;
+    });
+}
