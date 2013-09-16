2f00e6ca1294b30767a473356597b2bee2318419
wluyima <wyclif@openmrs.org>
2013-09-16 16:25:08 -0400

    Applied auto format changes

--- omod/src/main/java/org/openmrs/module/coreapps/page/controller/findpatient/FindPatientPageController.java
+++ omod/src/main/java/org/openmrs/module/coreapps/page/controller/findpatient/FindPatientPageController.java
@@ -10,19 +10,22 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  */
 public class FindPatientPageController {
-
-    /**
-     * This page is built to be shared across multiple apps. To use it, you must pass an "app" request parameter, which
-     * must be the id of an existing app that is an instance of coreapps.template.findPatient
-     * @param model
-     * @param app
-     */
-    public void get(PageModel model,
-                    @RequestParam("app") AppDescriptor app) {
-        model.addAttribute("afterSelectedUrl", app.getConfig().get("afterSelectedUrl").getTextValue());
-        model.addAttribute("heading", app.getConfig().get("label").getTextValue());
-        model.addAttribute("label", app.getConfig().get("label").getTextValue());
-        model.addAttribute("minSearchCharacters", Context.getAdministrationService().getGlobalProperty(OpenmrsConstants.GLOBAL_PROPERTY_MIN_SEARCH_CHARACTERS, "1"));
-    }
-
+	
+	/**
+	 * This page is built to be shared across multiple apps. To use it, you must pass an "app"
+	 * request parameter, which must be the id of an existing app that is an instance of
+	 * coreapps.template.findPatient
+	 * 
+	 * @param model
+	 * @param app
+	 */
+	public void get(PageModel model, @RequestParam("app") AppDescriptor app) {
+		model.addAttribute("afterSelectedUrl", app.getConfig().get("afterSelectedUrl").getTextValue());
+		model.addAttribute("heading", app.getConfig().get("label").getTextValue());
+		model.addAttribute("label", app.getConfig().get("label").getTextValue());
+		model.addAttribute("minSearchCharacters",
+		    Context.getAdministrationService()
+		            .getGlobalProperty(OpenmrsConstants.GLOBAL_PROPERTY_MIN_SEARCH_CHARACTERS, "1"));
+	}
+	
 }
