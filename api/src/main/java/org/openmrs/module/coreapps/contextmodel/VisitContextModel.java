a8a1cf73df21a159af8b8c5dc5737d25d639f424
pamcdm <pmori@pmori.local>
2013-12-27 16:18:14 -0200

    #958 - Add stopDatetime to visit context model

--- api/src/main/java/org/openmrs/module/coreapps/contextmodel/VisitContextModel.java
+++ api/src/main/java/org/openmrs/module/coreapps/contextmodel/VisitContextModel.java
@@ -2,6 +2,8 @@ package org.openmrs.module.coreapps.contextmodel;
 
 import org.openmrs.module.emrapi.visit.VisitDomainWrapper;
 
+import java.util.Date;
+
 /**
  * A very simple view of a visit, suitable for use in an app contextModel.
  */
@@ -10,11 +12,20 @@ public class VisitContextModel {
     private int id;
     private boolean active;
     private boolean admitted;
+    private Long stopDate;
 
     public VisitContextModel(VisitDomainWrapper visit) {
         this.id = visit.getVisitId();
         this.active = visit.isOpen();
         this.admitted = visit.isAdmitted();
+
+        Date stopDatetime = visit.getStopDatetime();
+
+        if(stopDatetime!=null){
+            this.stopDate = stopDatetime.getTime();
+        }else{
+            this.stopDate = 0L;
+        }
     }
 
     public int getId() {
@@ -27,6 +38,10 @@ public class VisitContextModel {
 
     public boolean isAdmitted() {
         return admitted;
+    }
+
+    public Long getStopDate(){
+        return  stopDate;
     }
 
 }
