962b4f29cfd8e08d09df8ef23fbe4b3e8a229c1d
mks-d <dimitri@mekomsolutions.com>
2016-11-03 17:57:45 +0100

    RA-1244: Basic non-Ajax paging on the Patient Dashboard.

--- api/src/main/java/org/openmrs/module/coreapps/CoreAppsProperties.java
+++ api/src/main/java/org/openmrs/module/coreapps/CoreAppsProperties.java
@@ -68,4 +68,8 @@ public class CoreAppsProperties extends ModuleProperties {
          return url;
       }
    }
+
+   public Integer getPatientDashboardEncounterCount() {
+      return getIntegerByGlobalProperty(CoreAppsConstants.GP_PATIENTDASHBOARD_ENCOUNTER_COUNT);
+   }
 }
