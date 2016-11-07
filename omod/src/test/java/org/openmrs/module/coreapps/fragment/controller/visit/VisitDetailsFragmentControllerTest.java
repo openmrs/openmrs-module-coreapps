8cc5b5950e37e193d79ea713f59524d922017375
mks-d <dimitri@mekomsolutions.com>
2016-11-07 11:17:47 +0100

    RA-1244: Adding .travis.yml + code reformatting.   new file:   .travis.yml   modified:   api/src/main/java/org/openmrs/module/coreapps/CoreAppsConstants.java   modified:   api/src/main/java/org/openmrs/module/coreapps/CoreAppsProperties.java   modified:   omod/src/main/java/org/openmrs/module/coreapps/fragment/controller/visit/VisitDetailsFragmentController.java   modified:   omod/src/main/java/org/openmrs/module/coreapps/page/controller/patientdashboard/PatientDashboardPageController.java   modified:   omod/src/main/webapp/resources/scripts/fragments/visitDetails.js   modified:   omod/src/test/java/org/openmrs/module/coreapps/fragment/controller/visit/VisitDetailsFragmentControllerTest.java

--- omod/src/test/java/org/openmrs/module/coreapps/fragment/controller/visit/VisitDetailsFragmentControllerTest.java
+++ omod/src/test/java/org/openmrs/module/coreapps/fragment/controller/visit/VisitDetailsFragmentControllerTest.java
@@ -13,6 +13,21 @@
  */
 package org.openmrs.module.coreapps.fragment.controller.visit;
 
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+
 import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.mockito.ArgumentMatcher;
@@ -42,166 +57,150 @@ import org.openmrs.ui.framework.UiFrameworkConstants;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.util.OpenmrsUtil;
 
-import java.text.ParseException;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-
-import static junit.framework.Assert.assertTrue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 public class VisitDetailsFragmentControllerTest {
 
-    private static final String encounterTypeUuid = "abc-123-def-456";
+   private static final String encounterTypeUuid = "abc-123-def-456";
 
-    private static final String primaryEncounterRoleUuid = "ghi-789-jkl-012";
+   private static final String primaryEncounterRoleUuid = "ghi-789-jkl-012";
 
-	@Test
-	public void shouldReturnEncountersForVisit() throws ParseException {
-		UiSessionContext sessionContext = mock(UiSessionContext.class);
-		User authenticatedUser = new User();
-		when(sessionContext.getCurrentUser()).thenReturn(authenticatedUser);
-        AppContextModel appContextModel = new AppContextModel();
-        when(sessionContext.generateAppContextModel()).thenReturn(appContextModel);
+   @Test
+   public void shouldReturnEncountersForVisit() throws ParseException {
+      UiSessionContext sessionContext = mock(UiSessionContext.class);
+      User authenticatedUser = new User();
+      when(sessionContext.getCurrentUser()).thenReturn(authenticatedUser);
+      AppContextModel appContextModel = new AppContextModel();
+      when(sessionContext.generateAppContextModel()).thenReturn(appContextModel);
 
-        authenticatedUser.addRole(createRoleForUser());
+      authenticatedUser.addRole(createRoleForUser());
 
-		AdministrationService administrationService = mock(AdministrationService.class);
-		when(administrationService.getGlobalProperty(UiFrameworkConstants.GP_FORMATTER_DATETIME_FORMAT)).thenReturn(
-		    "dd.MMM.yyyy, HH:mm:ss");
+      AdministrationService administrationService = mock(AdministrationService.class);
+      when(administrationService.getGlobalProperty(UiFrameworkConstants.GP_FORMATTER_DATETIME_FORMAT)).thenReturn(
+            "dd.MMM.yyyy, HH:mm:ss");
 
-        AppFrameworkService appFrameworkService = mock(AppFrameworkService.class);
-        when(appFrameworkService.getExtensionsForCurrentUser(CoreAppsConstants.ENCOUNTER_TEMPLATE_EXTENSION))
-                .thenReturn(generateMockEncounterTemplateExtensions());
+      AppFrameworkService appFrameworkService = mock(AppFrameworkService.class);
+      when(appFrameworkService.getExtensionsForCurrentUser(CoreAppsConstants.ENCOUNTER_TEMPLATE_EXTENSION))
+      .thenReturn(generateMockEncounterTemplateExtensions());
 
-        EncounterService encounterService = mock(EncounterService.class);
+      EncounterService encounterService = mock(EncounterService.class);
 
-        Patient patient = mock(Patient.class);
-        when(patient.getPatientId()).thenReturn(1);
-        when(patient.isDead()).thenReturn(false);
+      Patient patient = mock(Patient.class);
+      when(patient.getPatientId()).thenReturn(1);
+      when(patient.isDead()).thenReturn(false);
 
-		Visit visit = new Visit();
-		Location visitLocation = new Location();
-		visitLocation.setName("Visit Location");
-        visit.setVisitId(1);
-		visit.setLocation(visitLocation);
-		visit.setStartDatetime(new Date());
-		visit.setStopDatetime(new Date());
-        visit.setCreator(authenticatedUser);
-        visit.setPatient(patient);
-		Location encounterLocation = new Location();
-		encounterLocation.setName("Location");
-		EncounterType encounterType = new EncounterType();
-		encounterType.setName("Encounter Type");
-		encounterType.setUuid(encounterTypeUuid);
+      Visit visit = new Visit();
+      Location visitLocation = new Location();
+      visitLocation.setName("Visit Location");
+      visit.setVisitId(1);
+      visit.setLocation(visitLocation);
+      visit.setStartDatetime(new Date());
+      visit.setStopDatetime(new Date());
+      visit.setCreator(authenticatedUser);
+      visit.setPatient(patient);
+      Location encounterLocation = new Location();
+      encounterLocation.setName("Location");
+      EncounterType encounterType = new EncounterType();
+      encounterType.setName("Encounter Type");
+      encounterType.setUuid(encounterTypeUuid);
 
-        Provider primaryProvider = new Provider();
-		primaryProvider.setName("Primary Provider");
-		EncounterProvider primaryEncounterProvider = new EncounterProvider();
-		primaryEncounterProvider.setProvider(primaryProvider);
-        EncounterRole primaryEncounterRole = new EncounterRole();
-        primaryEncounterRole.setUuid(primaryEncounterRoleUuid);
-		primaryEncounterProvider.setEncounterRole(primaryEncounterRole);
+      Provider primaryProvider = new Provider();
+      primaryProvider.setName("Primary Provider");
+      EncounterProvider primaryEncounterProvider = new EncounterProvider();
+      primaryEncounterProvider.setProvider(primaryProvider);
+      EncounterRole primaryEncounterRole = new EncounterRole();
+      primaryEncounterRole.setUuid(primaryEncounterRoleUuid);
+      primaryEncounterProvider.setEncounterRole(primaryEncounterRole);
 
-        when(encounterService.getEncounterRoleByUuid(primaryEncounterRoleUuid)).thenReturn(primaryEncounterRole);
+      when(encounterService.getEncounterRoleByUuid(primaryEncounterRoleUuid)).thenReturn(primaryEncounterRole);
 
-        Provider secondaryProvider = new Provider();
-        secondaryProvider.setName("Secondary Provider");
-        EncounterProvider secondaryEncounterProvider = new EncounterProvider();
-        secondaryEncounterProvider.setProvider(secondaryProvider);
-        secondaryEncounterProvider.setEncounterRole(new EncounterRole());
-		
-		Encounter encounter = new Encounter();
-		encounter.setEncounterId(7);
-		encounter.setEncounterDatetime(new Date());
-		encounter.setLocation(encounterLocation);
-		encounter.setEncounterType(encounterType);
-		encounter.setEncounterProviders(new LinkedHashSet<EncounterProvider>());
-        encounter.getEncounterProviders().add(secondaryEncounterProvider);   // add secondary provider so that it is the one that is "arbitrarily" returned first
-        encounter.getEncounterProviders().add(primaryEncounterProvider);
-		encounter.setCreator(authenticatedUser);
-		
-		visit.addEncounter(encounter);
-		
-		UiUtils uiUtils = new TestUiUtils(administrationService);
-		VisitDetailsFragmentController controller = new VisitDetailsFragmentController();
-		
-		SimpleObject response = controller.getVisitDetails(mock(EmrApiProperties.class), appFrameworkService, encounterService,
-                visit, uiUtils, sessionContext);
-		List<SimpleObject> actualEncounters = (List<SimpleObject>) response.get("encounters");
-		SimpleObject actualEncounter = actualEncounters.get(0);
-		
-		assertThat(response.get("startDatetime"), notNullValue());
-		assertThat(response.get("stopDatetime"), notNullValue());
-		assertThat((String) response.get("location"), is("Visit Location"));
-		
-		assertThat(actualEncounters.size(), is(1));
-		assertThat((Integer) actualEncounter.get("encounterId"), is(7));
-		assertThat((String) actualEncounter.get("location"), is("Location"));
-		assertThat((SimpleObject) actualEncounter.get("encounterType"),
-		    isSimpleObjectWith("uuid", encounterType.getUuid(), "name", encounterType.getName()));
-		assertThat(actualEncounter.get("encounterDatetime"), notNullValue());
-		assertThat(actualEncounter.get("encounterDate"), notNullValue());
-		assertThat(actualEncounter.get("encounterTime"), notNullValue());
-        assertTrue((Boolean) actualEncounter.get("canEdit"));
-        assertThat((String) actualEncounter.get("primaryProvider"), is("Primary Provider"));
-		List<SimpleObject> actualProviders = (List<SimpleObject>) actualEncounter.get("encounterProviders");
-		assertThat(actualProviders.size(), is(2));
-	}
+      Provider secondaryProvider = new Provider();
+      secondaryProvider.setName("Secondary Provider");
+      EncounterProvider secondaryEncounterProvider = new EncounterProvider();
+      secondaryEncounterProvider.setProvider(secondaryProvider);
+      secondaryEncounterProvider.setEncounterRole(new EncounterRole());
 
-    private Role createRoleForUser() {
-        Role role = new Role();
-        role.setRole("Test");
-        role.addPrivilege(createPrivilegeToEditEncounters());
-        return role;
-    }
+      Encounter encounter = new Encounter();
+      encounter.setEncounterId(7);
+      encounter.setEncounterDatetime(new Date());
+      encounter.setLocation(encounterLocation);
+      encounter.setEncounterType(encounterType);
+      encounter.setEncounterProviders(new LinkedHashSet<EncounterProvider>());
+      encounter.getEncounterProviders().add(secondaryEncounterProvider);   // add secondary provider so that it is the one that is "arbitrarily" returned first
+      encounter.getEncounterProviders().add(primaryEncounterProvider);
+      encounter.setCreator(authenticatedUser);
 
-    private Privilege createPrivilegeToEditEncounters() {
-        Privilege privilege = new Privilege();
-        privilege.setPrivilege(EmrApiConstants.PRIVILEGE_EDIT_ENCOUNTER);
-        return privilege;
-    }
+      visit.addEncounter(encounter);
 
-    private Matcher<SimpleObject> isSimpleObjectWith(final Object... propertiesAndValues) {
-		return new ArgumentMatcher<SimpleObject>() {
-			
-			@Override
-			public boolean matches(Object o) {
-				SimpleObject so = (SimpleObject) o;
-				for (int i = 0; i < propertiesAndValues.length; i += 2) {
-					String property = (String) propertiesAndValues[i];
-					Object expectedValue = propertiesAndValues[i + 1];
-					if (!OpenmrsUtil.nullSafeEquals(so.get(property), expectedValue)) {
-						return false;
-					}
-				}
-				return true;
-			}
-		};
-	}
+      UiUtils uiUtils = new TestUiUtils(administrationService);
+      VisitDetailsFragmentController controller = new VisitDetailsFragmentController();
 
+      SimpleObject response = controller.getVisitDetails(mock(EmrApiProperties.class), appFrameworkService, encounterService,
+            visit, uiUtils, sessionContext);
+      List<SimpleObject> actualEncounters = (List<SimpleObject>) response.get("encounters");
+      SimpleObject actualEncounter = actualEncounters.get(0);
 
-    private List<Extension> generateMockEncounterTemplateExtensions() {
-        Extension extension = new Extension();
+      assertThat(response.get("startDatetime"), notNullValue());
+      assertThat(response.get("stopDatetime"), notNullValue());
+      assertThat((String) response.get("location"), is("Visit Location"));
 
-        Map<String,Object> encounterTypeParams = new HashMap<String, Object>();
-        encounterTypeParams.put("primaryEncounterRole", primaryEncounterRoleUuid);
+      assertThat(actualEncounters.size(), is(1));
+      assertThat((Integer) actualEncounter.get("encounterId"), is(7));
+      assertThat((String) actualEncounter.get("location"), is("Location"));
+      assertThat((SimpleObject) actualEncounter.get("encounterType"),
+            isSimpleObjectWith("uuid", encounterType.getUuid(), "name", encounterType.getName()));
+      assertThat(actualEncounter.get("encounterDatetime"), notNullValue());
+      assertThat(actualEncounter.get("encounterDate"), notNullValue());
+      assertThat(actualEncounter.get("encounterTime"), notNullValue());
+      assertTrue((Boolean) actualEncounter.get("canEdit"));
+      assertThat((String) actualEncounter.get("primaryProvider"), is("Primary Provider"));
+      List<SimpleObject> actualProviders = (List<SimpleObject>) actualEncounter.get("encounterProviders");
+      assertThat(actualProviders.size(), is(2));
+   }
 
-        Map<String, Object> supportedEncounterTypes = new HashMap<String, Object>();
-        supportedEncounterTypes.put(encounterTypeUuid, encounterTypeParams);
+   private Role createRoleForUser() {
+      Role role = new Role();
+      role.setRole("Test");
+      role.addPrivilege(createPrivilegeToEditEncounters());
+      return role;
+   }
 
-        Map<String, Object> extensionParams = new HashMap<String, Object>();
-        extensionParams.put("supportedEncounterTypes", supportedEncounterTypes);
+   private Privilege createPrivilegeToEditEncounters() {
+      Privilege privilege = new Privilege();
+      privilege.setPrivilege(EmrApiConstants.PRIVILEGE_EDIT_ENCOUNTER);
+      return privilege;
+   }
 
-        extension.setExtensionParams(extensionParams);
+   private Matcher<SimpleObject> isSimpleObjectWith(final Object... propertiesAndValues) {
+      return new ArgumentMatcher<SimpleObject>() {
 
-        return Collections.singletonList(extension);
-    }
+         @Override
+         public boolean matches(Object o) {
+            SimpleObject so = (SimpleObject) o;
+            for (int i = 0; i < propertiesAndValues.length; i += 2) {
+               String property = (String) propertiesAndValues[i];
+               Object expectedValue = propertiesAndValues[i + 1];
+               if (!OpenmrsUtil.nullSafeEquals(so.get(property), expectedValue)) {
+                  return false;
+               }
+            }
+            return true;
+         }
+      };
+   }
+
+   private List<Extension> generateMockEncounterTemplateExtensions() {
+      Extension extension = new Extension();
+
+      Map<String,Object> encounterTypeParams = new HashMap<String, Object>();
+      encounterTypeParams.put("primaryEncounterRole", primaryEncounterRoleUuid);
+
+      Map<String, Object> supportedEncounterTypes = new HashMap<String, Object>();
+      supportedEncounterTypes.put(encounterTypeUuid, encounterTypeParams);
+
+      Map<String, Object> extensionParams = new HashMap<String, Object>();
+      extensionParams.put("supportedEncounterTypes", supportedEncounterTypes);
+
+      extension.setExtensionParams(extensionParams);
+
+      return Collections.singletonList(extension);
+   }
 }
