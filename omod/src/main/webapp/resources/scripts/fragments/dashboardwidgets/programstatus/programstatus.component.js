be48e0b6debfe1f31df972c60c4de4a66ae55128
mgoodrich <mark.goodrich@live.com>
2017-04-12 14:41:26 -0400

    RA-1339: Program Enrollment Dashboard Widget work-in-progress

--- omod/src/main/webapp/resources/scripts/fragments/dashboardwidgets/programstatus/programstatus.component.js
+++ omod/src/main/webapp/resources/scripts/fragments/dashboardwidgets/programstatus/programstatus.component.js
@@ -1,4 +1,4 @@
-angular.module("openmrs-contrib-dashboardwidgets.programstatus", [ "openmrs-contrib-uicommons" ]).component('programstatus', {
+angular.module("openmrs-contrib-dashboardwidgets.programstatus", [ "openmrs-contrib-uicommons", "ui.bootstrap" ]).component('programstatus', {
     template: '<div ng-include="getTemplate()">',
     controller: ProgramStatusController,
     controllerAs: 'ctrl',
