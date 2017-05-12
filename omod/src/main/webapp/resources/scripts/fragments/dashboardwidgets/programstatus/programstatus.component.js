6045f3c68b942b02d62410fd53b116e989fe84db
mgoodrich <mark.goodrich@live.com>
2017-05-12 14:54:28 -0400

    RA-1363: Program Status Widget work-in-progress

--- omod/src/main/webapp/resources/scripts/fragments/dashboardwidgets/programstatus/programstatus.component.js
+++ omod/src/main/webapp/resources/scripts/fragments/dashboardwidgets/programstatus/programstatus.component.js
@@ -1,8 +1,9 @@
+
 // NOTE: work-in-progress, consider this not yet an "official" release of this widgets, future changes may not be backwards-compatible and change functionality signficantly
 
-angular.module("openmrs-contrib-dashboardwidgets.programs", [ "openmrs-contrib-uicommons" ]).component('programs', {
+angular.module("openmrs-contrib-dashboardwidgets.programstatus", [ "openmrs-contrib-uicommons", "ui.bootstrap"]).component('programstatus', {
     template: '<div ng-include="getTemplate()">',
-    controller: ProgramsController,
+    controller: ProgramStatusController,
     controllerAs: 'ctrl',
     bindings: {
         config: '<'
