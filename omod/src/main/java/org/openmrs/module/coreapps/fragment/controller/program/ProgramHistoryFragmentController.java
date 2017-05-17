79835d014e5e9d1cb883c240d5f177e3811b3878
mgoodrich <mark.goodrich@live.com>
2017-05-17 15:21:49 -0400

    RA-1367: Program History Dashboard Fragment

--- omod/src/main/java/org/openmrs/module/coreapps/fragment/controller/program/ProgramHistoryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/coreapps/fragment/controller/program/ProgramHistoryFragmentController.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.coreapps.fragment.controller.program;
 
+import groovy.lang.MissingPropertyException;
+import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 import org.openmrs.Patient;
@@ -49,16 +51,29 @@ public class ProgramHistoryFragmentController {
         ObjectNode appConfig = app.getConfig();
         appConfig.put("patientUuid", patientWrapper.getPatient().getUuid());
 
-        // TODO sort?
-        Program program = programWorkflowService.getProgramByUuid(appConfig.get("program").toString());
+        if (appConfig.get("program") == null || StringUtils.isEmpty(appConfig.get("program").getTextValue())) {
+            throw new MissingPropertyException("Program must be specified");
+        }
+
+        Program program = programWorkflowService.getProgramByUuid(appConfig.get("program").getTextValue());
+
+        if (program == null) {
+            throw new MissingPropertyException("Invalid program");
+        }
+
         List<PatientProgram> patientPrograms = programWorkflowService.getPatientPrograms(patientWrapper.getPatient(),
                 program, null, null, null, null, false);
 
+        // TODO: assumption here is that "getPatientPrograms" returns results in chronological order--need to confirm?
         Collections.reverse(patientPrograms);
         List<String> programJson = new ArrayList<String>();
 
+        Boolean includeActive = appConfig.get("includeActive") != null ? appConfig.get("includeActive").getBooleanValue() : true;
+        Boolean includeInActive = appConfig.get("includeInActive") != null ? appConfig.get("includeInActive").getBooleanValue() : true;
+
         for (PatientProgram patientProgram : patientPrograms) {
-            if (patientProgram.getDateCompleted() != null) {
+            if ((patientProgram.getDateCompleted() == null && includeActive)
+                || (patientProgram.getDateCompleted() != null && includeInActive)) {
                 appConfig.put("patientProgram", patientProgram.getUuid());
                 programJson.add(appConfig.toString().replace('\"', '\''));
             }
