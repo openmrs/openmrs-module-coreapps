746201c4334fb0ab4b23d112709e76f3a36aa104
mgoodrich <mark.goodrich@live.com>
2017-05-26 14:14:00 -0400

    RA-1362: Bundle dashboard widgets with webpack add ui.bootstrap dependency to relationships widget

--- omod/src/main/web/dashboardwidgets/relationships/index.js
+++ omod/src/main/web/dashboardwidgets/relationships/index.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import openmrsApi from '@openmrs/angularjs-openmrs-api';
+import uiBootstrap from 'angular-ui-bootstrap';
+
 import { RelationshipsComponent } from './relationships.component';
 
-export default angular.module("openmrs-contrib-dashboardwidgets.relationships", [ openmrsApi ])
+export default angular.module("openmrs-contrib-dashboardwidgets.relationships", [ openmrsApi, 'ui.bootstrap' ])
 	.component(RelationshipsComponent.selector, RelationshipsComponent).name;
