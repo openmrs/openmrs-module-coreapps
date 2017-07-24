5e67112febe526726e59ed1ed209229cc9278ce1
mgoodrich <mark.goodrich@live.com>
2017-07-24 16:12:13 -0400

    localize relationships widget

--- omod/src/main/web/dashboardwidgets/relationships/index.js
+++ omod/src/main/web/dashboardwidgets/relationships/index.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import openmrsApi from '@openmrs/angularjs-openmrs-api';
+import openmrsTranslate from '@openmrs/angularjs-openmrs-api';
 import typeahead from 'angular-ui-bootstrap/src/typeahead';
 
 import { RelationshipsComponent } from './relationships.component';
