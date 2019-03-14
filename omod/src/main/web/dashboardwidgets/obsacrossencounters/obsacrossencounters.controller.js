b9059da80260f0a6ec362fb19be9892727030e13
Cosmin Ioan <cioan@pih.org>
2019-03-14 10:20:35 -0400

    RA-1576, obsacrossencounters support for multiple obs within the same encounter

--- omod/src/main/web/dashboardwidgets/obsacrossencounters/obsacrossencounters.controller.js
+++ omod/src/main/web/dashboardwidgets/obsacrossencounters/obsacrossencounters.controller.js
@@ -1,117 +1,152 @@
 export default class ObsAcrossEncountersController {
-    constructor($filter, openmrsRest, widgetsCommons) {
-        'ngInject';
+  constructor($filter, openmrsRest, widgetsCommons) {
+    'ngInject';
 
-        Object.assign(this, { $filter, openmrsRest, widgetsCommons });
+    Object.assign(this, {$filter, openmrsRest, widgetsCommons});
+  }
+
+  $onInit() {
+    this.order = 'desc';
+    this.concepts = [];
+    // a map of conceptUUID --> concept(REST response)
+    this.conceptsMap = {};
+    this.simpleEncs = [];
+
+    this.openmrsRest.setBaseAppPath("/coreapps");
+
+    this.fetchConcepts();
+    this.fetchEncounters();
+  }
+
+  fetchConcepts() {
+    this.concepts = this.getConfigConceptsAsArray(this.config.concepts);
+    for (let i = 0; i < this.concepts.length; i++) {
+      this.openmrsRest.get("concept/" + this.concepts[i], {
+        v: 'custom:(uuid,display,names:(display,conceptNameType)'
+      }).then((concept) => {
+        let index = this.concepts.indexOf(concept.uuid);
+        this.concepts[index] = this.getConceptWithShortName(concept);
+        // update the concept map with the REST representation of the concept
+        this.conceptsMap[concept.uuid] = this.getConceptWithShortName(concept);
+      });
     }
+  }
 
-    $onInit() {
-        this.order = 'desc';
-        this.concepts = [];
-        this.encounters = [];
+  fetchEncounters() {
+    this.openmrsRest.get("encounter", {
+      patient: this.config.patientUuid,
+      encounterType: this.config.encounterType ? this.config.encounterType : null,
+      v: 'custom:(uuid,encounterDatetime,obs:(id,uuid,value,concept:(id,uuid,name:(display),datatype:(uuid)),groupMembers:(id,uuid,display,value,concept:(id,uuid,name:(display),datatype:(uuid))))',
+      limit: this.getMaxRecords(),
+      fromdate: this.widgetsCommons.maxAgeToDate(this.config.maxAge),
+      order: this.order
+    }).then((response) => {
+      this.parseEncounters(response.results);
+    });
+  }
 
-        this.openmrsRest.setBaseAppPath("/coreapps");
-
-        this.fetchConcepts();
-        this.fetchEncounters();
+  getMaxRecords() {
+    if (this.config.maxRecords == '' || angular.isUndefined(this.config.maxRecords)) {
+      return 4;
+    } else {
+      return this.config.maxRecords;
     }
+  }
 
-    fetchConcepts() {
-        this.concepts = this.getConfigConceptsAsArray(this.config.concepts);
-        for(let i = 0; i < this.concepts.length; i++) {
-            this.openmrsRest.getFull("concept/" + this.concepts[i], {v: 'custom:(uuid,display,names:(display,conceptNameType)'}).then((concept) => {
-                let index = this.concepts.indexOf(concept.uuid);
-                this.concepts[index] = this.getConceptWithShortName(concept);
-            });
+  displayObs(obs) {
+    let display = "";
+    if (obs.value != null) {
+      if (angular.isDefined(obs.value.display)) {
+        //If value is a concept
+        display = obs.value.display;
+      }
+      else if (['8d4a505e-c2cc-11de-8d13-0010c6dffd0f',
+          '8d4a591e-c2cc-11de-8d13-0010c6dffd0f',
+          '8d4a5af4-c2cc-11de-8d13-0010c6dffd0f'].indexOf(obs.concept.datatype.uuid) > -1) {
+        //If value is date, time or datetime
+        var date = this.$filter('date')(new Date(obs.value), this.config.dateFormat);
+        display = date;
+      } else {
+        display = obs.value;
+      }
+    }
+    return display;
+  }
+
+  getConfigConceptsAsArray(commaDelimitedConcepts) {
+    let conceptArray = commaDelimitedConcepts.replace(" ", "").split(",");
+    if (conceptArray !== null && conceptArray.length > 0) {
+      for (let i = 0; i < conceptArray.length; i++) {
+        let conceptKey = conceptArray[i];
+        // initialize the conceptsMap object
+        if (typeof this.conceptsMap[conceptKey] === 'undefined') {
+          this.conceptsMap[conceptKey] = null;
         }
+      }
     }
+    return conceptArray;
+  }
 
-    fetchEncounters() {
-        this.openmrsRest.get("encounter", {
-            patient: this.config.patientUuid,
-            v: 'custom:(uuid,encounterDatetime,obs:(uuid,value,concept:(uuid,datatype),groupMembers)',
-            limit: this.getMaxRecords(),
-            fromdate: this.widgetsCommons.maxAgeToDate(this.config.maxAge),
-            order: this.order
-        }).then((response) => {
-            this.getEncounters(response.results);
-        });
-    }
+  getConceptWithShortName(concept) {
+    angular.forEach(concept.names, (name) => {
+      if (name.conceptNameType == 'SHORT') {
+        concept.display = name.display;
+      }
+    });
+    return concept;
+  }
 
-    getMaxRecords() {
-        if(this.config.maxRecords == '' || angular.isUndefined(this.config.maxRecords)){
-            return 4;
-        } else {
-            return this.config.maxRecords;
+
+  parseEncounters(encounters) {
+    angular.forEach(encounters, (encounter) => {
+      let searchObs = {};
+      angular.forEach(encounter.obs, (obs) => {
+        let conceptKeys = Object.keys(this.conceptsMap);
+
+        if (conceptKeys.findIndex(conceptKey => conceptKey === obs.concept.uuid) >= 0) {
+          //we found an obs match
+          searchObs[obs.concept.uuid] = obs;
         }
-    }
-
-    getConfigConceptsAsArray(commaDelimitedConcepts) {
-        return commaDelimitedConcepts.replace(" ", "").split(",");
-    }
-
-    getConceptWithShortName(concept) {
-        angular.forEach(concept.names, (name) => {
-            if(name.conceptNameType == 'SHORT'){
-                concept.display = name.display;
-            }
-        });
-        return concept;
-    }
-
-    getEncounters(encounters) {
-        angular.forEach(encounters, (encounter) => {
-            let enc = {
+        if (obs.groupMembers != null && obs.groupMembers.length > 0) {
+          // need to search the groupMembers
+          let foundObs = this.parseGroupMembers(obs.groupMembers, conceptKeys);
+          if (typeof foundObs !== 'undefined' && foundObs !== null && Object.keys(foundObs).length > 0) {
+            if (Object.keys(foundObs).every(item => conceptKeys.includes(item))) {
+              //this is a complete pair of matching obs with a given concept uuid within the same obsGroup
+              let enc = {
                 encounterDatetime: encounter.encounterDatetime,
-                obs: []
-            };
-            var nullRow = true;
-            angular.forEach(this.getConfigConceptsAsArray(this.config.concepts), (concept) => {
-                let obsValue = this.getObservationForConcept(encounter.obs, concept);
-                enc.obs.push(obsValue);
-                if(obsValue.value !== '-') {
-                    nullRow = false;
-                }
-            });
-
-            if(!nullRow) {
-                this.encounters.push(enc);
+                obs: foundObs
+              };
+              this.simpleEncs.push(enc);
+            } else {
+              // we have an incomplete match, just add the values to the existing object
+              for (key in Object.keys(foundObs)){
+                searchObs[key] = searchObs[key];
+              }
             }
-        });
-    }
-
-    getObservationForConcept(observations, conceptUuid) {
-        for(var i = 0; i < observations.length; i++){
-            var obs = observations[i];
-            if (obs.groupMembers != null) {
-                if (obs.groupMembers.length !== 0) {
-                    //it is a group obs
-                    for (var j = 0; j < obs.groupMembers.length; j++) {
-                        var groupMember = obs.groupMembers[j];
-                        if (groupMember.concept.uuid === conceptUuid) {
-                            obs = groupMember;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            if (obs.value != null && obs.concept.uuid === conceptUuid) {
-                if (angular.isDefined(obs.value.display)) {
-                    //If value is a concept
-                    obs.value = obs.value.display;
-                }
-                else if (['8d4a505e-c2cc-11de-8d13-0010c6dffd0f',
-                        '8d4a591e-c2cc-11de-8d13-0010c6dffd0f',
-                        '8d4a5af4-c2cc-11de-8d13-0010c6dffd0f'].indexOf(obs.concept.datatype.uuid) > -1) {
-                    //If value is date, time or datetime
-                    var date = this.$filter('date')(new Date(obs.value), this.config.dateFormat);
-                    obs.value = date;
-                }
-
-                return obs;
-            }
+          }
         }
-        return {value: '-'};
-    }
+      });
+
+      if (Object.keys(searchObs).length > 0) {
+        let tempEnc = {
+          encounterDatetime: encounter.encounterDatetime,
+          obs: searchObs
+        };
+        this.simpleEncs.push(tempEnc);
+      }
+    });
+  }
+
+  parseGroupMembers(groupMembers, concepts) {
+    let matchObs = {};
+    angular.forEach(groupMembers, (obs) => {
+      if (concepts.includes(obs.concept.uuid)) {
+        //we found an obs match
+        matchObs[obs.concept.uuid] = obs;
+      }
+    });
+    return matchObs;
+  }
+
 }
