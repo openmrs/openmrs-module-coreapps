9c56da0caf19737dd639ca44b1022b8f2d8d72c5
unknown <haripriya.reddy999@gmail.com>
2019-07-03 15:18:29 +0530

    backup:end date validation

--- omod/src/main/webapp/resources/scripts/conditionlist/controllers/addcondition.controller.js
+++ omod/src/main/webapp/resources/scripts/conditionlist/controllers/addcondition.controller.js
@@ -9,6 +9,7 @@ function ConditionController($scope, RestfulService, ConditionModel, ConceptMode
 
     $scope.patientUuid = null;
     $scope.condition = null;
+    const INACTIVE_STATUS = 'INACTIVE';  
 
     // this is required inorder to initialize the Restangular service
     RestfulService.setBaseUrl('/' + OPENMRS_CONTEXT_PATH + '/ws/rest/emrapi');
@@ -20,6 +21,10 @@ function ConditionController($scope, RestfulService, ConditionModel, ConceptMode
      * @type {Function}
      */
     self.saveCondition = self.saveCondition || function () {
+        if($scope.condition.onSetDate > $scope.condition.endDate){
+            emr.errorAlert("End Date can't be before Onset Date");
+        }
+        else{
             var conditions = [];
             conditions.push($scope.condition);
             RestfulService.post('condition', conditions, function (data) {
@@ -31,34 +36,62 @@ function ConditionController($scope, RestfulService, ConditionModel, ConceptMode
                 emr.errorAlert("Error Saving condition");
             });
         }
+    }
 
     self.initCondition = self.initCondition || function () {
             $scope.patientUuid = CommonFunctions.extractUrlArgs(window.location.search)['patientId'];
             $scope.condition = new ConditionModel($scope.patientUuid);
+            self.showEndDate();
         }
 
     self.validateCondition = self.validateCondition || function () {
-            var concept = $scope.concept.concept;
-            if (concept !== null) {
-                $scope.condition.concept = new ConceptModel(concept.uuid, concept.display);
-            } else {
-                // noncoded diagnosis
-                $scope.condition.conditionNonCoded = "NON_CODED:" + $scope.concept.word;
-            }
-
-            $scope.condition.onSetDate = self.getSelectedDate();
-
-            self.saveCondition();
+        var concept ;
+        if($scope.concept.concept.uuid) {
+            concept=$scope.concept.concept;
+        } else {
+            concept=$scope.condition.concept;
         }
+
+        if (concept !== null) {
+            $scope.condition.concept = new ConceptModel(concept.uuid, concept.display);
+        } else {
+            // noncoded diagnosis
+            $scope.condition.conditionNonCoded = "NON_CODED:" + $scope.concept.word;
+        }
+
+        $scope.condition.onSetDate = self.getSelectedDate();
+
+        if($scope.condition.status==INACTIVE_STATUS) {
+            $scope.condition.endDate = self.getEndDate();
+        }
+        self.saveCondition();
+    }
+
+    self.showEndDate = self.showEndDate || function() {
+        var groups = document.getElementsByClassName("group");
+        if($scope.condition.status==INACTIVE_STATUS){
+            groups[2].style.visibility = "visible";
+        }
+        else{
+            groups[2].style.visibility = "hidden";    
+        }
+     }        
 
     self.getSelectedDate = self.getSelectedDate || function () {
             var datePicker = angular.element(document.getElementsByName('conditionStartDate'))[0];
             return datePicker.value;
         }
 
+    self.getEndDate = self.getEndDate || function() {
+            var endDatePicker = angular.element(document.getElementsByName('conditionEndDate'))[0];
+            return endDatePicker.value;
+        } 
+
     // init page
     self.initCondition();
 
     // bind functions to scope
     $scope.validateCondition = self.validateCondition;
+    $scope.showEndDate = self.showEndDate;
+    $scope.getEndDate = self.getEndDate;
 }
