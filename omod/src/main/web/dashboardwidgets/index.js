232dec141017843e76dc4243484365aa34d389e8
mozzy11 <mozzymutesa@gmail.com>
2019-11-08 16:22:40 +0300

    RA-1638 : Create new (Bahmni) Appointments widget in Core Apps

--- omod/src/main/web/dashboardwidgets/index.js
+++ omod/src/main/web/dashboardwidgets/index.js
@@ -9,9 +9,10 @@ import ProgramStatistics from './programstatistics';
 import ProgramStatus from './programstatus';
 import Relationships from './relationships';
 import VisitByEncounterType from './visitbyencountertype';
+import Appointments from './appointments';
 
 export default angular.module("openmrs-contrib-dashboardwidgets", [ DataIntegrityViolations, LatestObsForConceptList,
-    ObsAcrossEncounters, ObsGraph, Programs, ProgramStatistics, ProgramStatus, Relationships, VisitByEncounterType]).name;
+    ObsAcrossEncounters, ObsGraph, Programs, ProgramStatistics, ProgramStatus, Relationships, VisitByEncounterType ,Appointments]).name;
 
 
 angular.element(document).ready(function() {
