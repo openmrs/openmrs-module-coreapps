5f15deffbd09115cbb502055979477026a7cbb20
Cosmin Ioan <cioan@pih.org>
2019-12-19 10:26:39 -0500

    RA-1666, configure obsacrossencounters column names

--- omod/src/main/web/dashboardwidgets/obsacrossencounters/index.js
+++ omod/src/main/web/dashboardwidgets/obsacrossencounters/index.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 import openmrsApi from '@openmrs/angularjs-openmrs-api';
+import openmrsTranslate from '@openmrs/angularjs-openmrs-api';
 import commons from './../dashboardwidgets.services';
 
 import { ObsAcrossEncountersComponent } from './obsacrossencounters.component';
 
-export default angular.module("openmrs-contrib-dashboardwidgets.obsacrossencounters", [ openmrsApi, commons ])
+export default angular.module("openmrs-contrib-dashboardwidgets.obsacrossencounters", [ openmrsApi, openmrsTranslate, commons ])
 	.component(ObsAcrossEncountersComponent.selector, ObsAcrossEncountersComponent).name;
