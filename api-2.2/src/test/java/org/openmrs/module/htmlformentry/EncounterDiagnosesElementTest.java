80cec78e6c350b3642bf5689e4b2401acc9a005b
samuelmale <samuelsmalek@gmail.com>
2020-01-28 13:17:15 +0300

    RA-1694: EncounterDiagnosis widget to only display diagnoses of the current encounter and not all patient's past diagnoses

---
+++ api-2.2/src/test/java/org/openmrs/module/htmlformentry/EncounterDiagnosesElementTest.java
@@ -0,0 +1,52 @@
+package org.openmrs.module.htmlformentry;
+
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.openmrs.CodedOrFreeText;
+import org.openmrs.Diagnosis;
+import org.openmrs.Encounter;
+import org.openmrs.module.coreapps.htmlformentry.EncounterDiagnosesElement;
+import org.openmrs.module.htmlformentry.FormEntryContext.Mode;
+import org.openmrs.test.BaseContextMockTest;
+
+public class EncounterDiagnosesElementTest extends BaseContextMockTest {
+
+    @Mock
+	FormEntryContext context;
+    
+    @Mock
+    Encounter existingEncounter;
+    
+    @Before
+	public void setup() {
+		when(context.getExistingEncounter()).thenReturn(existingEncounter);
+		when(context.getMode()).thenReturn(Mode.EDIT);
+		
+	}
+	
+	@Test
+	public void testGetExistingDiagnoses() {
+		// Setup
+		when(existingEncounter.getDiagnoses()).thenReturn(
+                new HashSet<Diagnosis>(Arrays.asList(new Diagnosis(existingEncounter, new CodedOrFreeText(null, null, "Asthma"), null, null, null), 
+                		new Diagnosis(existingEncounter, new CodedOrFreeText(null, null, "Malaria"), null, null, null))));
+
+		// Replay
+		EncounterDiagnosesElement element = new EncounterDiagnosesElement();
+		Set<Diagnosis> existingDiangnoses = element.getExistingDiagnoses(context);
+		
+		// Verify
+		Assert.assertEquals(2, existingDiangnoses.size());
+		Assert.assertEquals("Malaria", ((Diagnosis) existingDiangnoses.toArray()[0]).getDiagnosis().getNonCoded());
+		Assert.assertEquals("Asthma", ((Diagnosis) existingDiangnoses.toArray()[1]).getDiagnosis().getNonCoded());
+
+	}
+}
