39fbd3d8fabc057abb741ba91204bee49fa2e13e
Pedro Sousa <pedroedrasousa@gmail.com>
2020-07-27 15:55:41 +0100

    Consider voided attribute when converting the core diagnosis model object list to the emrapi model

--- api-2.2/src/test/java/org/openmrs/module/htmlformentry/EncounterDiagnosesElementTest.java
+++ api-2.2/src/test/java/org/openmrs/module/htmlformentry/EncounterDiagnosesElementTest.java
@@ -37,51 +37,69 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(Context.class)
 public class EncounterDiagnosesElementTest {
-	
+
 	private Concept concept;
-	
+
 	private ConceptSource source;
-	
+
 	private String sourceName;
 
     @Mock
 	FormEntryContext context;
-    
+
     @Mock
     Encounter existingEncounter;
-    
+
     @Mock
     ConceptService conceptService;
-    
+
     @Mock
     private EmrApiProperties emrApiProperties;
-    
+
     @Before
 	public void setup() {
     	PowerMockito.mockStatic(Context.class);
     	when(context.getExistingEncounter()).thenReturn(existingEncounter);
     	when(Context.getConceptService()).thenReturn(conceptService);
 	}
-	
+
 	@Test
 	public void testGetExistingDiagnoses() {
 		// Setup
+		Diagnosis asthmaDiagnosis = new Diagnosis(existingEncounter, new CodedOrFreeText(null, null, "Asthma"), null, null, null);
+		Diagnosis malariaDiagnosis = new Diagnosis(existingEncounter, new CodedOrFreeText(null, null, "Malaria"), null, null, null);
+		Diagnosis voidedEpilepsyDiagnosis = new Diagnosis(existingEncounter, new CodedOrFreeText(null, null, "Epilepsy"), null, null, null);
+		voidedEpilepsyDiagnosis.setVoided(true);
 		when(context.getMode()).thenReturn(Mode.EDIT);
 		when(existingEncounter.getDiagnoses()).thenReturn(
-                new LinkedHashSet<Diagnosis>(Arrays.asList(new Diagnosis(existingEncounter, new CodedOrFreeText(null, null, "Asthma"), null, null, null), 
-                		new Diagnosis(existingEncounter, new CodedOrFreeText(null, null, "Malaria"), null, null, null))));
+				new LinkedHashSet<Diagnosis>(Arrays.asList(asthmaDiagnosis, malariaDiagnosis, voidedEpilepsyDiagnosis)));
 
 		// Replay
 		EncounterDiagnosesElement element = new EncounterDiagnosesElement();
-		Set<Diagnosis> existingDiangnoses = element.getExistingDiagnoses(context);
-		
-		// Verify
-		Assert.assertEquals(2, existingDiangnoses.size());
-		Assert.assertEquals("Asthma", ((Diagnosis) existingDiangnoses.toArray()[0]).getDiagnosis().getNonCoded());
-		Assert.assertEquals("Malaria", ((Diagnosis) existingDiangnoses.toArray()[1]).getDiagnosis().getNonCoded());
+		Set<Diagnosis> existingDiagnoses = element.getExistingDiagnoses(context);
 
+		// Verify
+		Assert.assertEquals(2, existingDiagnoses.size());
+
+		int asthmaDiagnosisCount = 0;
+		int malariaDiagnosisCount = 0;
+		int voidedEpilepsyDiagnosisCount = 0;
+		for (Diagnosis diagnosis : existingDiagnoses) {
+			if ("Asthma".equals(diagnosis.getDiagnosis().getNonCoded())) {
+				asthmaDiagnosisCount++;
+			}
+			if ("Malaria".equals(diagnosis.getDiagnosis().getNonCoded())) {
+				malariaDiagnosisCount++;
+			}
+			if ("Epilepsy".equals(diagnosis.getDiagnosis().getNonCoded())) {
+				voidedEpilepsyDiagnosisCount++;
+			}
+		}
+		Assert.assertEquals(asthmaDiagnosisCount, 1);
+		Assert.assertEquals(malariaDiagnosisCount, 1);
+		Assert.assertEquals(voidedEpilepsyDiagnosisCount, 0);
 	}
-	
+
 	@Test
 	public void generateHtml_shouldCodeDiagnosisUsingPreferredCodingSource() {
 		// Setup
@@ -91,15 +109,15 @@ public class EncounterDiagnosesElementTest {
 		EncounterDiagnosesElement element = new EncounterDiagnosesElement();
 		element.setUiUtils(new BasicUiUtils());
 		element.setPreferredCodingSource(sourceName);
-		
+
 		when(context.getMode()).thenReturn(Mode.VIEW);
 		when(existingEncounter.getDiagnoses()).thenReturn(
                 new LinkedHashSet<Diagnosis>(Arrays.asList(new Diagnosis(existingEncounter, new CodedOrFreeText(concept, null, null), ConditionVerificationStatus.CONFIRMED, null, null))));
 		when(conceptService.getConceptSourceByName(source.getName())).thenReturn(source);
-		
+
 		// Replay
 		String generatedHtml = element.generateHtml(context);
-		
+
 		// Verify
 		Mockito.verify(conceptService, Mockito.times(1)).getConceptSourceByName(source.getName());
 		Assert.assertEquals("<p>"
@@ -107,7 +125,7 @@ public class EncounterDiagnosesElementTest {
 		                    + "<span>(coreapps.Diagnosis.Certainty.CONFIRMED) Malaria [Pref-code-1234]</span>"
 		                    + "</p>", generatedHtml);
 	}
-	
+
 	@Test
 	public void generateHtml_shouldCodeDiagnosisEmrApiPropertiesDiagnosisSources() {
 		// Setup
@@ -117,16 +135,16 @@ public class EncounterDiagnosesElementTest {
 		EncounterDiagnosesElement element = new EncounterDiagnosesElement();
 		element.setUiUtils(new BasicUiUtils());
 		element.setEmrApiProperties(emrApiProperties);
-		
+
 		when(context.getMode()).thenReturn(Mode.VIEW);
 		when(existingEncounter.getDiagnoses()).thenReturn(
                 new LinkedHashSet<Diagnosis>(Arrays.asList(new Diagnosis(existingEncounter, new CodedOrFreeText(concept, null, null), ConditionVerificationStatus.CONFIRMED, null, null))));
 		when(emrApiProperties.getConceptSourcesForDiagnosisSearch()).thenReturn(Arrays.asList(source));
-		
+
 		// Replay
 		element.setPreferredCodingSource(null);
 		String generatedHtml = element.generateHtml(context);
-		
+
 		// Verify
 		Mockito.verify(emrApiProperties, Mockito.times(1)).getConceptSourcesForDiagnosisSearch();
 		Assert.assertEquals("<p>"
@@ -134,12 +152,12 @@ public class EncounterDiagnosesElementTest {
 		                    + "<span>(coreapps.Diagnosis.Certainty.CONFIRMED) Malaria [ICD-WHO]</span>"
 		                    + "</p>", generatedHtml);
 	}
-	
+
 	private void setupDiagnosis(String sourceName, String code) {
-		
+
 		source = new ConceptSource();
 		source.setName(sourceName);
-		
+
 		ConceptReferenceTerm referenceTerm = new ConceptReferenceTerm();
 		referenceTerm.setConceptSource(source);
 		referenceTerm.setCode(code);
@@ -147,23 +165,23 @@ public class EncounterDiagnosesElementTest {
 		ConceptMapType mapType = new ConceptMapType();
 		mapType.setName("Occurrence");
 		mapType.setUuid(EmrApiConstants.SAME_AS_CONCEPT_MAP_TYPE_UUID);
-		
+
 		ConceptMap conceptMap = new ConceptMap();
 		conceptMap.setConceptReferenceTerm(referenceTerm);
 		conceptMap.setConceptMapType(mapType);
-		
+
 		ConceptName conceptName = new ConceptName();
 		concept = new Concept();
 		concept.addConceptMapping(conceptMap);
 		concept.addName(conceptName);
-		
+
 		conceptName.setConcept(concept);
 		conceptName.setName("Malaria");
 		conceptName.setLocalePreferred(true);
 		conceptName.setLocale(Locale.ENGLISH);
 
 	}
-	
+
 	private class BasicUiUtils extends UiUtils {
 
 		public BasicUiUtils() {
@@ -174,7 +192,7 @@ public class EncounterDiagnosesElementTest {
 		public String message(String property, Object... args) {
 			return property;
 		}
-		
+
 		@Override
 		public Locale getLocale() {
 			return Locale.ENGLISH;
