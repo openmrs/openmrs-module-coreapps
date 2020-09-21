openmrs/openmrs-module-coreapps

commit 4546d08e7ac2bf85978b947c84822efc0e8f8cba

Make ctrl-click on a patient program link open in a new tab (#359)

omod/src/main/web/dashboardwidgets/programs/programs.controller.js
@@ -136,12 +136,9 @@ export default class ProgramsController {
         this.input.program = "";
     }
 
-   gotoProgramDashboard(programUuid) {
+   gotoProgramDashboard(programUuid, event) {
 
-        if (!programUuid) {
-            programUuid = this.input.program
-        }
-
+        programUuid = programUuid || this.input.program;
         if (programUuid && this.config.enableProgramDashboards) {
             var destinationPage = "";
             destinationPage = Handlebars.compile(this.dashboardPage)({
@@ -149,7 +146,12 @@ export default class ProgramsController {
                 dashboard: programUuid
             });
             this.openmrsRest.getServerUrl().then((url) => {
-                window.location.href = url + destinationPage;
+                const target = url + destinationPage;
+                if (!event.metaKey && !event.ctrlKey && !event.shiftKey && event.button == 0) {
+                    window.location.href = target;
+                } else {
+                    window.open(target, '_blank');
+                }
             });
         }
     }

omod/src/main/web/dashboardwidgets/programs/programs.html
@@ -18,7 +18,7 @@
 <ul>
     <li ng-show="$ctrl.patientPrograms.length != 0" ng-repeat="patientProgram in $ctrl.patientPrograms | orderBy: '-dateEnrolled'">
         <!-- href="#" included so bootstrap will style this as a link -->
-        <a href="#" ng-click="$ctrl.gotoProgramDashboard(patientProgram.program.uuid)">
+        <a href="#" ng-click="$ctrl.gotoProgramDashboard(patientProgram.program.uuid, $event)">
             {{ patientProgram.program.display }}
         </a>
 

commit 0b0f53f6ce02da417247a02a3474525f5ddc818d

RA-1530 Program List dashboard links don't behave like real links: take 2
This fixes an issue introduced in 4546d08e7ac2bf85978b947c84822efc0e8f8cba
which causes the "Enroll" button not to work.

https://issues.openmrs.org/browse/RA-1530

omod/src/main/web/dashboardwidgets/programs/programs.controller.js
@@ -136,7 +136,7 @@ export default class ProgramsController {
         this.input.program = "";
     }
 
-   gotoProgramDashboard(programUuid, event) {
+   gotoProgramDashboard(event, programUuid) {
 
         programUuid = programUuid || this.input.program;
         if (programUuid && this.config.enableProgramDashboards) {
@@ -147,7 +147,8 @@ export default class ProgramsController {
             });
             this.openmrsRest.getServerUrl().then((url) => {
                 const target = url + destinationPage;
-                if (!event.metaKey && !event.ctrlKey && !event.shiftKey && event.button == 0) {
+                console.log(event);
+                if (!event.metaKey && !event.ctrlKey && !event.shiftKey) {
                     window.location.href = target;
                 } else {
                     window.open(target, '_blank');

omod/src/main/web/dashboardwidgets/programs/programs.html
@@ -18,7 +18,7 @@
 <ul>
     <li ng-show="$ctrl.patientPrograms.length != 0" ng-repeat="patientProgram in $ctrl.patientPrograms | orderBy: '-dateEnrolled'">
         <!-- href="#" included so bootstrap will style this as a link -->
-        <a href="#" ng-click="$ctrl.gotoProgramDashboard(patientProgram.program.uuid, $event)">
+        <a href="#" ng-click="$ctrl.gotoProgramDashboard($event, patientProgram.program.uuid)">
             {{ patientProgram.program.display }}
         </a>
 
@@ -48,7 +48,7 @@
         <br/>
     </div>
     <div>
-        <button id="enroll-button" class="confirm" ng-click="$ctrl.gotoProgramDashboard()">{{'coreapps.enroll' | translate }}</button>
+        <button id="enroll-button" class="confirm" ng-click="$ctrl.gotoProgramDashboard($event)">{{'coreapps.enroll' | translate }}</button>
         <button id="cancel-button" class="cancel" ng-click="$ctrl.cancelAddProgram()">{{'coreapps.cancel' | translate }}</button>
     </div>
 </div>