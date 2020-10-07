bf636cc1339832a0bdce56b9c4b38de9c52aa973
Pahonsi Bebeto <39851246+achilep@users.noreply.github.com>
2020-10-07 13:31:09 +0100

    RA-1821 Unnecessary imports should be removed (#343)

--- omod/src/main/java/org/openmrs/module/coreapps/fragment/controller/DiagnosesFragmentController.java
+++ omod/src/main/java/org/openmrs/module/coreapps/fragment/controller/DiagnosesFragmentController.java
@@ -15,7 +15,6 @@
 package org.openmrs.module.coreapps.fragment.controller;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
