8aa5633ecf6863990b1e1a31808bc5f7050d0261
icrc-ifernandes <65180726+icrc-ifernandes@users.noreply.github.com>
2021-02-16 20:36:54 +0000

    RA-1857: Added WidgetsCommons#formatDate for L10n.

--- omod/src/main/web/dashboardwidgets/programstatus/index.js
+++ omod/src/main/web/dashboardwidgets/programstatus/index.js
@@ -2,8 +2,9 @@ import angular from 'angular';
 import openmrsApi from '@openmrs/angularjs-openmrs-api';
 import openmrsTranslate from '@openmrs/angularjs-openmrs-api';
 import datepicker from './../datepicker';
+import commons from './../dashboardwidgets.services';
 
 import { ProgramStatusComponent } from './programstatus.component';
 
-export default angular.module("openmrs-contrib-dashboardwidgets.programstatus", [ openmrsApi, datepicker, openmrsTranslate ])
+export default angular.module("openmrs-contrib-dashboardwidgets.programstatus", [ openmrsApi, datepicker, openmrsTranslate, commons ])
 	.component(ProgramStatusComponent.selector, ProgramStatusComponent).name;
