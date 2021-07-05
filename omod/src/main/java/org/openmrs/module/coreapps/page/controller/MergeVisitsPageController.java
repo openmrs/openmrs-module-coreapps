22fc4958971f4ac2edf525965496fc64236ca976
icrc-jfigueiredo <72515287+icrc-jfigueiredo@users.noreply.github.com>
2021-07-05 22:32:27 +0100

    RA-1926: Merge Visits: Remove visitId from URL to prevent NPE if that visit was merged (#410)
    * RA-1926 Remove visitId from URL to prevent NPE if that visit was merged
    
    * RA-1926 Remove visit ID if that visit was merged
    
    * RA-1926 Add new method to remove URL visits if necessary
    
    * RA-1926 Add new test file, and 2 new tests, to remove url if that visit id was merged
    
    * RA-1926 Change MergeVisitsPageControllerTest.java file to correct package
    
    * RA-1926 Change variables names
    
    * RA-1926 Indentation
    
    * RA-1926 Add comment
    
    * RA-1926 Changed var names
    
    * RA-1926 Change tests MergeVisitsPageControllerTest
    
    * RA-1926 Changed method to protected
    
    * RA-1926 Renaming variables
    
    * RA-1926 Indentation
    
    * RA-1926 Change StringUtils Library

--- omod/src/main/java/org/openmrs/module/coreapps/page/controller/MergeVisitsPageController.java
+++ omod/src/main/java/org/openmrs/module/coreapps/page/controller/MergeVisitsPageController.java
@@ -15,18 +15,24 @@ import org.openmrs.ui.framework.annotation.InjectBeans;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.page.PageModel;
 import org.openmrs.ui.framework.page.Redirect;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MergeVisitsPageController {
     public Object controller(@RequestParam("patientId") Patient patient,
-                    @InjectBeans PatientDomainWrapper patientDomainWrapper,
-                    UiSessionContext sessionContext,
-                    PageModel model, @SpringBean AdtService service,
-                    @SpringBean("locationService") LocationService locationService,
-					@RequestParam(value = "returnUrl", required = false) String returnUrl) {
+                             @InjectBeans PatientDomainWrapper patientDomainWrapper,
+                             UiSessionContext sessionContext,
+                             PageModel model, @SpringBean AdtService service,
+                             @SpringBean("locationService") LocationService locationService,
+                             @RequestParam(value = "returnUrl", required = false) String returnUrl,
+                             @RequestParam(value = "mergedVisitIds", required = false) List<String> mergedVisitIds) {
+
 
         if (patient.isVoided() || patient.isPersonVoided()) {
             return new Redirect("coreapps", "patientdashboard/deletedPatient", "patientId=" + patient.getId());
@@ -34,7 +40,13 @@ public class MergeVisitsPageController {
 
         patientDomainWrapper.setPatient(patient);
         model.addAttribute("patient", patientDomainWrapper);
-		model.addAttribute("returnUrl", returnUrl);
+
+        //Remove the visit ID from URL, in case we merge that visit, it should not be possible to return to it.
+        if (!StringUtils.isEmpty(returnUrl) && returnUrl.contains("visitId=") && mergedVisitIds.size() > 0) {
+            returnUrl = removeVisitIdFromURLIfVisitWasMerged(returnUrl, mergedVisitIds);
+        }
+
+        model.addAttribute("returnUrl", returnUrl);
 
         Location sessionLocation = sessionContext.getSessionLocation();
         Location visitLocation = null;
@@ -52,27 +64,54 @@ public class MergeVisitsPageController {
 
     public String post(@RequestParam("patientId") Patient patient,
                        @RequestParam("mergeVisits") List<Integer> mergeVisits,
-					   @RequestParam(value = "returnUrl", required = false) String returnUrl,
+                       @RequestParam(value = "returnUrl", required = false) String returnUrl,
                        @SpringBean AdtService service,
                        UiUtils ui,
-                       HttpServletRequest request, PageModel model){
+                       HttpServletRequest request, PageModel model) {
 
+        List<String> mergedVisitIds = new ArrayList<String>();
         if (patient.isVoided() || patient.isPersonVoided()) {
             return "redirect:" + ui.pageLink("coreapps", "patientdashboard/deletedPatient",
-					SimpleObject.create("patientId", patient.getId().toString() ));
+                    SimpleObject.create("patientId", patient.getId().toString()));
         }
 
-        if (mergeVisits!=null && mergeVisits.size() > 0 ){
+        if (mergeVisits != null && mergeVisits.size() > 0) {
             Visit mergedVisit = service.mergeConsecutiveVisits(mergeVisits, patient);
-            if (mergedVisit != null){
+            if (mergedVisit != null) {
                 request.getSession().setAttribute(AppUiConstants.SESSION_ATTRIBUTE_INFO_MESSAGE, ui.message("coreapps.task.mergeVisits.success"));
                 request.getSession().setAttribute(AppUiConstants.SESSION_ATTRIBUTE_TOAST_MESSAGE, "true");
-            }else{
+            } else {
                 request.getSession().setAttribute("emr.errorMessage", ui.message("coreapps.task.mergeVisits.error"));
                 request.getSession().setAttribute(AppUiConstants.SESSION_ATTRIBUTE_TOAST_MESSAGE, "true");
             }
-        }
 
-		return "redirect:" + ui.pageLink("coreapps", "mergeVisits", SimpleObject.create("patientId", patient.getId(), "returnUrl", returnUrl));
+            //Pass by all visits except the first one.
+            for (int i = 1; i < mergeVisits.size(); i++) {
+                mergedVisitIds.add(mergeVisits.get(i).toString());
+            }
+        }
+        return "redirect:" + ui.pageLink("coreapps", "mergeVisits", SimpleObject.create("patientId", patient.getId(), "returnUrl", returnUrl, "mergedVisitIds", mergedVisitIds));
+    }
+
+
+    /**
+     * If some of the merged visit IDs are on the return url, it scraps the visitId param altogether off the URL
+     * so it doesn't return to a visit that no longer exists.
+     */
+    protected String removeVisitIdFromURLIfVisitWasMerged(String returnUrl, List<String> mergedVisitsIDs) {
+        //Pattern to get the visitID from url
+        Pattern pat = Pattern.compile("(?<=visitId=)[^&]+");
+        Matcher urlVisitId = pat.matcher(returnUrl);
+        //If find some visitID on url.
+        if (urlVisitId.find()) {
+            for (String mergedVisitId : mergedVisitsIDs.get(0).split(",")) {
+                if (urlVisitId.group(0).equals(mergedVisitId)) {
+                    //remove the visitID from url
+                    returnUrl = returnUrl.replaceAll("(&visitId[^&]+)", "");
+                    break;
+                }
+            }
+        }
+        return returnUrl;
     }
 }
---
+++ omod/src/test/java/org/openmrs/module/coreapps/page/controller/MergeVisitsPageControllerTest.java
@@ -0,0 +1,50 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ * <p>
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * <p>
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.coreapps.page.controller;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MergeVisitsPageControllerTest {
+
+    MergeVisitsPageController controller;
+
+    private final String RETURN_URL_WITH_VISIT_ID = "http://localhost:8080/openmrs/coreapps/patientdashboard/patientDashboard.page?patientId=9e121f61-457f-4eaa-9121-62fbb9acdaad&visitId=7";
+    private final String RETURN_URL_WITHOUT_VISIT_ID = "http://localhost:8080/openmrs/coreapps/patientdashboard/patientDashboard.page?patientId=9e121f61-457f-4eaa-9121-62fbb9acdaad";
+
+    @Before
+    public void setup() {
+        controller = new MergeVisitsPageController();
+
+    }
+
+    @Test
+    public void test_shouldRemoveVisitParameterFromReturnURL() {
+        List<String> merged_visits_ids = Arrays.asList("6,7,8,9");
+        Assert.assertEquals(controller.removeVisitIdFromURLIfVisitWasMerged(RETURN_URL_WITH_VISIT_ID, merged_visits_ids), RETURN_URL_WITHOUT_VISIT_ID);
+    }
+
+    @Test
+    public void test_shouldNotRemoveVisitParameterFromReturnURL() {
+        List<String> merged_visits_ids = Arrays.asList("3,4,5,6");
+        Assert.assertEquals(controller.removeVisitIdFromURLIfVisitWasMerged(RETURN_URL_WITH_VISIT_ID, merged_visits_ids), RETURN_URL_WITH_VISIT_ID);
+    }
+
+}
