8db15a2ab16cbeeb6dedbd10fdb85a518df89b24
Joel Figueiredo <jfigueiredo@icrc.org>
2021-09-13 08:50:51 +0100

    RA-1954 - Change GP name

--- api/src/main/java/org/openmrs/module/coreapps/CoreAppsConstants.java
+++ api/src/main/java/org/openmrs/module/coreapps/CoreAppsConstants.java
@@ -59,5 +59,5 @@ public class CoreAppsConstants {
 
    public static final String DEFAULT_CODING_SOURCE = "ICD-10-WHO";
 
-   public static final String DECEASED_DATE_USING_TIME = "coreapps.deceasedDateUsingTime";
+   public static final String GP_DECEASED_DATE_USING_TIME = "coreapps.deceasedDateUsingTime";
 }
