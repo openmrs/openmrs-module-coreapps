570609dbef413aaff9af983c6e20c123518d35ab
Cosmin <cioan@pih.org>
2021-09-21 15:19:43 -0400

    RA-1960, order obs by datetime descending (#431)

--- omod/src/main/web/dashboardwidgets/obsacrossencounters/obsacrossencounters.controller.js
+++ omod/src/main/web/dashboardwidgets/obsacrossencounters/obsacrossencounters.controller.js
@@ -93,7 +93,8 @@ export default class ObsAcrossEncountersController {
     this.output.rows = [];
     let encounterDateToDisplay;
     let providersNameToDisplay;
-    const sortedEncs = this.simpleEncs.sort((a, b) => a.encounterDatetime - b.encounterDatetime);
+    // sort in descending order of encounterDatetime
+    const sortedEncs = this.simpleEncs.sort((a, b) => (new Date(b.encounterDatetime)).getTime() - (new Date(a.encounterDatetime)).getTime());
     for (let i = 0; i < sortedEncs.length; i++) {
       encounterDateToDisplay = "";
       providersNameToDisplay = "";
